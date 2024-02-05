eec5b23e32ae8d69c2a0d6b8ec437e51db8a7b40
Natan <Th3than@gmail.com>
2024-02-05 13:53:55 +0700

    Move player to package Players and add Crew

---
+++ Players/Player.java
@@ -0,0 +1,14 @@
+package Players;
+
+public class Player {
+    long budget = 1000; //Players.Player’s budget
+    public  Player(){
+
+    }
+    public Player(long budget){
+        this.budget = budget;
+    }
+//    Crew MyCrew;Players.Player’s crew
+//    Region citycenter; Players.Player’s citycenter
+//    LinkedList<Region> Myregion;Regions are belonged by Players.Player
+}
