50a9861e6e648360b4b21abcb26fef869d491445
Prem Bannawat <bannawat_v@cmu.ac.th>
2024-02-11 02:01:36 +0700

    make evaluator no-op when user assign special variables

--- src/Parser/AssignIdentifier.java
+++ src/Parser/AssignIdentifier.java
@@ -5,7 +5,11 @@ import java.util.Map;
 public record AssignIdentifier(String key, Expr val) implements State {
     @Override
     public void eval(Map<String, Integer> bindings) throws Exception {
+        boolean isNotSpecial = switch (key) {
+            case "rows", "cols", "currow", "curcol", "budget", "deposit", "int", "maxdeposit", "random" -> false;
+            default -> true;
+        };
         if(!bindings.containsKey(key)) bindings.put(key,0);
-        bindings.put(key, val.eval(bindings));
+        if(isNotSpecial) bindings.put(key, val.eval(bindings)); //no-op
     }
 }
