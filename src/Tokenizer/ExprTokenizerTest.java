6be84d4e644526b4244b47884a028bc8e0e9bbea
Prem Bannawat <bannawat_v@cmu.ac.th>
2024-02-11 02:15:27 +0700

    create and manage text files for testing

---
+++ src/Tokenizer/ExprTokenizerTest.java
@@ -0,0 +1,27 @@
+package Tokenizer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExprTokenizerTest {
+    @Test
+    void acceptNumber(){
+
+    }
+
+    @Test
+    void acceptOperator(){
+
+    }
+
+    @Test
+    void acceptCorrectWord(){
+
+    }
+
+    @Test
+    void supportEndOfLineComments(){
+
+    }
+}
