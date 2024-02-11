7142814616fa29c786d763c8c13d4c6d339bcdaa
Prem Bannawat <bannawat_v@cmu.ac.th>
2024-02-11 20:57:18 +0700

    add Junit test for Tokenizer

--- src/Tokenizer/Main.java
+++ src/Tokenizer/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws LexicalError, IOException, SyntaxError {
-        PlanTokenizer exr = new PlanTokenizer("src/Tokenizer/TestTokenPlaintext/sampleCons.txt");
+        PlanTokenizer exr = new PlanTokenizer("src/Tokenizer/TestTokenPlaintext/unknownChar.txt");
         while (exr.hasNextToken()){
             System.out.println(exr.consume());
         }
