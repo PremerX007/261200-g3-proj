998da77c5734645c077684e74e9bfbc66e1d85e6
Prem Bannawat <bannawat_v@cmu.ac.th>
2024-02-11 13:32:07 +0700

    change BlockStatement and WhileStatement from record to class (fix static queue)

--- src/Parser/BlockStatement.java
+++ src/Parser/BlockStatement.java
@@ -4,12 +4,14 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 
-public record BlockStatement() implements GroupState {
-    private static Queue<State> q = new LinkedList<>();
+public class BlockStatement implements GroupState {
+    private Queue<State> q = new LinkedList<>();
     @Override
     public void eval(Map<String, Integer> bindings) throws Exception {
-        for(State s : q){
-            s.eval(bindings);
+        if(!q.isEmpty()){
+            for(State s : q){
+                s.eval(bindings);
+            }
         }
     }
 
--- src/Parser/WhileStatement.java
+++ src/Parser/WhileStatement.java
@@ -4,8 +4,13 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Queue;
 
-public record WhileStatement(Expr expr) implements GroupState {
-    private static Queue<State> q = new LinkedList<>();
+public class WhileStatement implements GroupState {
+    private Expr expr;
+    private Queue<State> q = new LinkedList<>();
+    public WhileStatement(Expr expr){
+        this.expr = expr;
+    }
+
     @Override
     public void eval(Map<String, Integer> bindings) throws Exception {
         for (int counter = 0; counter < 10000 && expr.eval(bindings) > 0; counter++){
