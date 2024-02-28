0d7aa46804ce9ecabb6d7fe48524ba5e8d35ed18
Prem Bannawat <bannawat_v@cmu.ac.th>
2024-02-28 12:46:52 +0700

    modify parameter of AssignIdentifier record

--- src/Parser/Statement/AssignIdentifier.java
+++ src/Parser/Statement/AssignIdentifier.java
@@ -2,12 +2,13 @@ package Parser.Statement;
 
 import Parser.EvalError;
 import Parser.Expression.Expr;
+import Controller.Player;
 
 import java.util.Map;
 
 public record AssignIdentifier(String key, Expr val) implements Statement {
     @Override
-    public void eval(Map<String, Long> bindings) throws EvalError {
+    public void eval(Player player, Map<String, Long> bindings) throws EvalError {
         boolean isNotSpecial = switch (key) {
             case "rows", "cols", "currow", "curcol", "budget", "deposit", "int", "maxdeposit", "random" -> false;
             default -> true;
@@ -15,8 +16,7 @@ public record AssignIdentifier(String key, Expr val) implements Statement {
         if(isNotSpecial){ //no-op
             if(!bindings.containsKey(key))
                 bindings.put(key, 0L);
-
-            bindings.put(key, val.eval(bindings));
+            bindings.put(key, val.eval(player, bindings));
         }
     }
 }
