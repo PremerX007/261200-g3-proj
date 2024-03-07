1352f188e52864ea229660c9205159dd6543d1d5
Prem Bannawat <bannawat_v@cmu.ac.th>
2024-03-07 20:51:47 +0700

    initial GamePage

--- upbeat/src/App.tsx
+++ upbeat/src/App.tsx
@@ -3,17 +3,17 @@ import LoginPage from "./components/LoginPage";
 import WaitingRoom from "./components/WaitingRoom";
 import { useAppSelector } from "./store/hooks";
 import { selectWebSocket } from "./store/Slices/webSocketSlice";
+import GamePage from "./components/GamePage";
 
 function App() {
   const webSocketState = useAppSelector(selectWebSocket);
   return (
-    <div className="h-screen flex flex-col justify-center items-center">
-      <div className="w-full h-1/2 bg-blue-500"></div>
-      <div className="w-full h-1/2 bg-white"></div>
-      {webSocketState.isConnected ? <WaitingRoom /> : <LoginPage />}
-      {/* <LoginPage /> */}
-      {/* <WaitingRoom /> */}
-    </div>
+    // <div className="h-screen flex flex-col justify-center items-center">
+    //   <div className="w-full h-1/2 bg-blue-500"></div>
+    //   <div className="w-full h-1/2 bg-white"></div>
+    //   {webSocketState.isConnected ? <WaitingRoom /> : <LoginPage />}
+    // </div>
+    <GamePage />
   );
 }
 
